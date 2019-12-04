2e8181b514e7189208a007660698816e91689321
Cavan Farrell <cavanf@withoutwire.com>
2019-12-04 12:12:53 -0600

    adding back in day 2 part 1/2

--- Program.cs
+++ Program.cs
@@ -23,8 +23,8 @@ namespace AdventOfCode
             var codeList_Part1 = Day04_SecureContainer.GetValidCodesInRange_Part1(min, max);
             var codeList_Part2 = Day04_SecureContainer.GetValidCodesInRange_Part2(min, max);
 
-            Console.WriteLine($"Day 04, Part1: Secure Container: {codeList_Part1.Count} valid codes");
-            Console.WriteLine($"Day 04, Part2: Secure Container: {codeList_Part2.Count} valid codes");
+            Console.WriteLine($"Day 04, Part 1: Secure Container: {codeList_Part1.Count} valid codes");
+            Console.WriteLine($"Day 04, Part 2: Secure Container: {codeList_Part2.Count} valid codes");
         }
 
         static void Day03()
@@ -42,14 +42,17 @@ namespace AdventOfCode
 
         static void Day02()
         {
-            var program = new List<int> { 1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,1,10,19,2,6,19,23,1,23,5,
+            var program = new List<int> { 1,12,2,3,1,1,2,3,1,3,4,3,1,5,0,3,2,1,10,19,2,6,19,23,1,23,5,
                 27,1,27,13,31,2,6,31,35,1,5,35,39,1,39,10,43,2,6,43,47,1,47,5,51,1,51,9,55,2,55,6,59,1,59,
                 10,63,2,63,9,67,1,67,5,71,1,71,5,75,2,75,6,79,1,5,79,83,1,10,83,87,2,13,87,91,1,10,91,95,
                 2,13,95,99,1,99,9,103,1,5,103,107,1,107,10,111,1,111,5,115,1,115,6,119,1,119,10,123,1,123,
                 10,127,2,127,13,131,1,13,131,135,1,135,10,139,2,139,6,143,1,143,9,147,2,147,6,151,1,5,151,
                 155,1,9,155,159,2,159,6,163,1,163,2,167,1,10,167,0,99,2,14,0,0
             };
-
+            
+            var firstProgram = new List<int>();
+            firstProgram.AddRange(program);
+            Console.WriteLine($"Day 02, Part 1: Gravity Assist: {Day02_1202ProgramAlarm.RunProgram(firstProgram)[0]}");
             for (int noun = 0; noun <= 99; noun++)
             {
                 for (int verb = 0; verb <= 99; verb++)
@@ -61,7 +64,7 @@ namespace AdventOfCode
                     var output = Day02_1202ProgramAlarm.RunProgram(newProgram)[0];
                     if (output == 19690720)
                     {
-                        Console.WriteLine($"Day 02: Gravity Assist: {100 * noun + verb} noun/verb");
+                        Console.WriteLine($"Day 02, Part 2: Gravity Assist: {100 * noun + verb} noun/verb");
                     }
                 }
             }
